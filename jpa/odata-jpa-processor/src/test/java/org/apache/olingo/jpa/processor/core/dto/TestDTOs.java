ca2bbbfce47b5386b504c94320456ab1d10db48b
Ralf Zozmann <github@oxote.de>
2019-12-12 15:45:17 +0100

    - refactoring test

--- jpa/odata-jpa-processor/src/test/java/org/apache/olingo/jpa/processor/core/dto/TestDTOs.java
+++ jpa/odata-jpa-processor/src/test/java/org/apache/olingo/jpa/processor/core/dto/TestDTOs.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import org.apache.olingo.client.api.uri.URIBuilder;
 import org.apache.olingo.commons.api.ex.ODataException;
 import org.apache.olingo.commons.api.http.HttpMethod;
 import org.apache.olingo.commons.api.http.HttpStatusCode;
@@ -22,66 +23,72 @@ import org.junit.Test;
 
 public class TestDTOs extends TestBase {
 
-	@Before
-	public void setup() throws ODataJPAModelException {
-		persistenceAdapter.registerDTO(EnvironmentInfo.class);
-		persistenceAdapter.registerDTO(SystemRequirement.class);
-	}
+  @Before
+  public void setup() throws ODataJPAModelException {
+    persistenceAdapter.registerDTO(EnvironmentInfo.class);
+    persistenceAdapter.registerDTO(SystemRequirement.class);
+  }
 
-	@Test(expected = ODataJPAModelException.class)
-	public void testNonDTOThrowsError() throws IOException, ODataException, SQLException {
-		// create own instance to avoid pollution of other tests
-		final TestGenericJPAPersistenceAdapter myPersistenceAdapter = new TestGenericJPAPersistenceAdapter(
-		        Constant.PUNIT_NAME,
-		        DataSourceHelper.DatabaseType.HSQLDB);
-		myPersistenceAdapter.registerDTO(TestDTOs.class);
-		// must throw an exception on further processing
-		final IntegrationTestHelper helper = new IntegrationTestHelper(myPersistenceAdapter, "$metadata");
-		helper.execute(HttpStatusCode.OK.getStatusCode());
-	}
+  @Test(expected = ODataJPAModelException.class)
+  public void testNonDTOThrowsError() throws IOException, ODataException, SQLException {
+    // create own instance to avoid pollution of other tests
+    final TestGenericJPAPersistenceAdapter myPersistenceAdapter = new TestGenericJPAPersistenceAdapter(
+        Constant.PUNIT_NAME,
+        DataSourceHelper.DatabaseType.HSQLDB);
+    myPersistenceAdapter.registerDTO(TestDTOs.class);
+    // must throw an exception on further processing
+    final URIBuilder uriBuilder = newUriBuilder().appendMetadataSegment();
+    final IntegrationTestHelper helper = new IntegrationTestHelper(myPersistenceAdapter, uriBuilder);
+    helper.execute(HttpStatusCode.OK.getStatusCode());
+  }
 
-	@Test
-	public void testDTOMetadata() throws IOException, ODataException, SQLException {
+  @Test
+  public void testDTOMetadata() throws IOException, ODataException, SQLException {
 
-		final IntegrationTestHelper helper = new IntegrationTestHelper(persistenceAdapter, "$metadata");
-		helper.execute(HttpStatusCode.OK.getStatusCode());
-		final String xml = helper.getRawResult();
-		assertTrue(!xml.isEmpty());
-		assertTrue(xml.contains(EnvironmentInfo.class.getSimpleName()));
-		assertTrue(xml.contains(EnvironmentInfo.class.getSimpleName().concat("s")));// + entity set
-	}
+    final URIBuilder uriBuilder = newUriBuilder().appendMetadataSegment();
+    final IntegrationTestHelper helper = new IntegrationTestHelper(persistenceAdapter, uriBuilder);
+    helper.execute(HttpStatusCode.OK.getStatusCode());
+    final String json = helper.getRawResult();
+    assertTrue(!json.isEmpty());
+    assertTrue(json.contains(EnvironmentInfo.class.getSimpleName()));
+    assertTrue(json.contains(EnvironmentInfo.class.getSimpleName().concat("s")));// + entity set
+  }
 
-	@Test
-	public void testGetDTO() throws IOException, ODataException, SQLException {
+  @Test
+  public void testGetDTO() throws IOException, ODataException, SQLException {
 
-		final IntegrationTestHelper helper = new IntegrationTestHelper(persistenceAdapter,
-		        "EnvironmentInfos");
-		helper.execute(HttpStatusCode.OK.getStatusCode());
-		assertTrue(helper.getValues().size() > 0);
-		assertEquals(System.getProperty("java.version"), helper.getValues().get(0).get("JavaVersion").asText());
-	}
+    final URIBuilder uriBuilder = newUriBuilder().appendEntitySetSegment("EnvironmentInfos");
+    final IntegrationTestHelper helper = new IntegrationTestHelper(persistenceAdapter, uriBuilder);
+    helper.execute(HttpStatusCode.OK.getStatusCode());
+    assertTrue(helper.getValues().size() > 0);
+    assertEquals(System.getProperty("java.version"), helper.getValues().get(0).get("JavaVersion").asText());
+  }
 
-	@Test
-	public void testGetSpecificDTO() throws IOException, ODataException, SQLException {
-		// our example DTO handler will not support loading of a DTO with a specific ID
-		final IntegrationTestHelper helper = new IntegrationTestHelper(persistenceAdapter, "EnvironmentInfos(1)");
-		helper.execute(HttpStatusCode.INTERNAL_SERVER_ERROR.getStatusCode());
-	}
+  @Test
+  public void testGetSpecificDTO() throws IOException, ODataException, SQLException {
+    // our example DTO handler will not support loading of a DTO with a specific ID
+    final URIBuilder uriBuilder = newUriBuilder().appendEntitySetSegment("EnvironmentInfos").appendKeySegment(Integer
+        .valueOf(1));
+    final IntegrationTestHelper helper = new IntegrationTestHelper(persistenceAdapter, uriBuilder);
+    helper.execute(HttpStatusCode.INTERNAL_SERVER_ERROR.getStatusCode());
+  }
 
-	@Test
-	public void testWriteDTO() throws IOException, ODataException, SQLException {
-		final String id = Integer.toString((int) System.currentTimeMillis());
-		final StringBuffer requestBody = new StringBuffer("{");
-		requestBody.append("\"EnvNames\": [\"envA\"], ");
-		requestBody.append("\"JavaVersion\": \"0.0.ex\", ");
-		requestBody.append("\"Id\": " + id);
-		requestBody.append("}");
+  @Test
+  public void testWriteDTO() throws IOException, ODataException, SQLException {
+    final int iId = (int) System.currentTimeMillis();
+    final String sId = Integer.toString(iId);
+    final StringBuffer requestBody = new StringBuffer("{");
+    requestBody.append("\"EnvNames\": [\"envA\"], ");
+    requestBody.append("\"JavaVersion\": \"0.0.ex\", ");
+    requestBody.append("\"Id\": " + sId);
+    requestBody.append("}");
 
-		final IntegrationTestHelper helper = new IntegrationTestHelper(persistenceAdapter,
-		        "EnvironmentInfos(" + id + ")",
-		        requestBody, HttpMethod.PUT);
-		helper.execute(HttpStatusCode.OK.getStatusCode());
-		assertEquals(id, helper.getValue().get("Id").asText());
-	}
+    final URIBuilder uriBuilder = newUriBuilder().appendEntitySetSegment("EnvironmentInfos").appendKeySegment(Integer
+        .valueOf(iId));
+    final IntegrationTestHelper helper = new IntegrationTestHelper(persistenceAdapter, uriBuilder,
+        requestBody, HttpMethod.PUT);
+    helper.execute(HttpStatusCode.OK.getStatusCode());
+    assertEquals(sId, helper.getValue().get("Id").asText());
+  }
 
 }
